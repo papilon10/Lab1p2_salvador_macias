26d54309a39f6a1df1584a8290b94174a18a8af0
papilon10 <salvigna17@gmail.com>
2025-04-25 13:40:42 -0600

    menu vacio

--- src/lab1p2_salvador_macias/Lab1p2_salvador_macias.java
+++ src/lab1p2_salvador_macias/Lab1p2_salvador_macias.java
@@ -4,17 +4,48 @@
  */
 package lab1p2_salvador_macias;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
  *
  * @author Apple
  */
 public class Lab1p2_salvador_macias {
 
+    public static Scanner lea = new Scanner(System.in);
+    public static Scanner str = new Scanner(System.in);
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        boolean salida = false;
+        do {
+            System.out.println("---menu principal---");
+            System.out.println("1.jugar numberle");
+            System.out.println("2.verificar palindromo");
+            System.out.println("3.salir");
+            System.out.println("elija una opcion: ");
+            int opc = lea.nextInt();
+            switch (opc) {
+                case 1:{}
+                    
+                    break;
+                case 2:{}
+                
+                break;
+                
+                case 3:{}
+                
+                break;
+                default:
+                    System.out.println("la opcion ingresada es invalida");
+            }
+            
+            
+        } while (salida != true);
+
     }
-    
+
 }
