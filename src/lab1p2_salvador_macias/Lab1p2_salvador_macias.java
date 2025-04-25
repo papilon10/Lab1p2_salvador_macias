37b9ff78999de42eb471343a0d8370000fbc6c2e
papilon10 <salvigna17@gmail.com>
2025-04-25 14:33:14 -0600

    avanze numberle facil

--- src/lab1p2_salvador_macias/Lab1p2_salvador_macias.java
+++ src/lab1p2_salvador_macias/Lab1p2_salvador_macias.java
@@ -6,6 +6,7 @@ package lab1p2_salvador_macias;
 
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.Random;
 
 /**
  *
@@ -15,7 +16,8 @@ public class Lab1p2_salvador_macias {
 
     public static Scanner lea = new Scanner(System.in);
     public static Scanner str = new Scanner(System.in);
-    public static ArrayList lista = new ArrayList();
+    public static ArrayList historial_intento = new ArrayList();
+    public static ArrayList historial_retroAlimentacion = new ArrayList();
 
     /**
      * @param args the command line arguments
@@ -36,14 +38,35 @@ public class Lab1p2_salvador_macias {
                     System.out.println("2. media (intentos 6,longitud 4, rango 1-8)");
                     System.out.println("3. dificil (intentos 5,longitud 5, rango 1-9)");
                     boolean acierto = false;
-                    System.out.println("elija la dificultad: ");
+                    System.out.println("\nelija la dificultad: ");
                     int dificultad = lea.nextInt();
                     switch (dificultad) {
                         case 1: {
                             while (acierto != true) {
-                                for (int i = 0; i < 8; i++) {
-                                    System.out.println("intento numero :" + i);
+                                Random aleatorio = new Random();
+                                StringBuilder sb = new StringBuilder();
 
+                                for (int y = 0; y < 3; y++) {
+                                    int digito = aleatorio.nextInt(6) + 1;
+                                    sb.append(digito);
+                                }
+
+                                String numeroAleatorio = sb.toString();
+                                System.out.println(numeroAleatorio);
+                                for (int i = 1; i < 9; i++) {
+                                    System.out.println("intento numero :" + i);
+                                    String numero_ingresado = str.nextLine();
+                                    historial_intento.add(numero_ingresado);
+
+                                    if (numeroAleatorio.contains(numero_ingresado)) {
+                                        acierto=true;
+                                        System.out.println("historial: ");
+                                        for (int j = 0; j < historial_intento.size(); j++) {
+                                            System.out.println(historial_intento.get(j));
+
+                                        }
+
+                                    }
                                 }
 
                             }
@@ -75,31 +98,29 @@ public class Lab1p2_salvador_macias {
 
                     break;
 
-                }
+                }//fin case 1
+                case 2: {
 
-            }
-
-            break;
-          case 2: {
-                }
+                }//fin case 2
 
                 break;
 
                 case 3: {
                     salida = true;
                     System.out.println("se abandonara el programa");
-                }
+                }//fina case 2
 
                 break;
-                default:
+                default: {
                     System.out.println("la opcion ingresada es invalida");
-            }
+                }
+            }//fin switch
 
-        
-    }
-    while (salida 
-!= true);
+            break;
 
-    }
+        } while (salida
+                != true);
 
-}
+    }//fin main
+
+}//fin clase
